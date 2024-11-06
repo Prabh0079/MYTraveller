057963dca2989a05b4606caff813af65ed397eaa
Prabh0079 <prabneha8487@gmail.com>
2024-11-06 17:23:13 -0500

    modify

--- app/src/main/java/com/example/mytraveller/MainActivity.java
+++ app/src/main/java/com/example/mytraveller/MainActivity.java
@@ -1,17 +1,77 @@
 package com.example.mytraveller;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.mytraveller.databinding.ActivityMainBinding;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class MainActivity extends BaseActivity {
+    private ActivityMainBinding binding;
+    private int adultPassenger = 1, childPassenger = 1;
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("d MMM, yyyy", Locale.ENGLISH);
+    private Calendar calendar = Calendar.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        initLocations();
+
+
     }
+
+
+    private void initLocations() {
+        binding.progressBarFrom.setVisibility(View.VISIBLE);
+        binding.progressBarTo.setVisibility(View.VISIBLE);
+        DatabaseReference myRef = database.getReference("Locations");
+        ArrayList<Location> list = new ArrayList<>();
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    for (DataSnapshot issue : snapshot.getChildren()) {
+                        list.add(issue.getValue(Location.class));
+                    }
+                    ArrayAdapter<Location> adapter = new ArrayAdapter<>(MainActivity.this, R.layout.sp_item, list);
+                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    binding.fromSp.setAdapter(adapter);
+                    binding.toSp.setAdapter(adapter);
+                    binding.fromSp.setSelection(1);
+                    binding.progressBarFrom.setVisibility(View.GONE);
+                    binding.progressBarTo.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+
 }
